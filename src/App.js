f9eba491caa89f3a2c6bd826531087053d659bab
Doğan Can Selek <57669626+cselekk97@users.noreply.github.com>
2021-01-01 23:27:52 +0300

    edit
    çalışmayan swapi nin kalıntıları düzeltildi :)))

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react'
 import './App.css';
 import { useQuery, gql } from '@apollo/client'
 
-const ALL_STARSHIPS = gql`
+const ALL_COUNTRIES = gql`
   {
     countries{
       name
@@ -15,13 +15,13 @@ const ALL_STARSHIPS = gql`
 
 
 function App() {
-  const { loading, error, data } = useQuery(ALL_STARSHIPS);
+  const { loading, error, data } = useQuery(ALL_COUNTRIES);
   if (loading) return <p>Yükleniyor...</p>
   if (error) return <p>Gene yanlış...</p>
 
   return (
     <Fragment>
-      <h1>All Starships</h1>
+      <h1>All Countries</h1>
       {data.countries.map((countries, id) => (
         <div key={id}>
           <p> <b>Ülke Adı:</b> {countries.name}</p>
